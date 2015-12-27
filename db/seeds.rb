46132d2c146fe1bee68f647b82add2aa93b9cf11
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-12-27 17:38:55 -0500

    Created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ingredients = ["Chicken", "Beef", "Spinach", "Mushrooms", "Pasta", "Tomato", "Carrot", "Onion"]
+
+ingredients.each do |ingredient|
+  Ingredient.create(
+    name: ingredient
+  )
+end
+
+20.times do
+  recipe = Recipe.create(
+    name: Faker::Lorem.words.join(' '),
+    servings: 1 + rand(4),
+    cook_time: "#{rand(180)} minutes",
+    ingredient_lines: []
+  )
+
+  4.times do
+    offset = rand(Ingredient.count)
+    recipe.ingredients << Ingredient.offset(offset).first
+  end
+end
+
+
+10.times do
+  Blog.create(
+    name: Faker::Lorem.words.join(' ')
+  )
+end
