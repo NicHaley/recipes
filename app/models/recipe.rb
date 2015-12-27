7370fc8c1b656c489f5f6bec7554cf56e14bdd86
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-12-27 17:25:02 -0500

    Created ingredients model and ingredients-recipes join table

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ActiveRecord::Base
 	belongs_to :blog
 	has_many :images, as: :imageable
+	has_and_belongs_to_many :ingredients
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227221831) do
+ActiveRecord::Schema.define(version: 20151227222326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,17 @@ ActiveRecord::Schema.define(version: 20151227221831) do
     t.string   "imageable_type"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients_recipes", id: false, force: :cascade do |t|
+    t.integer "ingredient_id", null: false
+    t.integer "recipe_id",     null: false
   end
 
   create_table "recipes", force: :cascade do |t|
