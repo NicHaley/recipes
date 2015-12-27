53f54e30fed24cf748807b5e249066f794cebe91
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-12-27 17:20:43 -0500

    Created polymorphic image table

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ActiveRecord::Base
 	belongs_to :blog
+	has_many :images, as: :imageable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227221356) do
+ActiveRecord::Schema.define(version: 20151227221831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20151227221356) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "imageable_id"
+    t.string   "imageable_type"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "recipes", force: :cascade do |t|
