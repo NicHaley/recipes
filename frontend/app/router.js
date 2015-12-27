0bb0d7be66b91b215cf74804cb8d33fd2c1e4203
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-12-27 16:58:04 -0500

    Created routes on frontend

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
+var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
+  this.route('recipes', { path: '/' }, function() {
+    this.route('recipe', {path: 'recipes/:id'});
+  });
+  this.route('blogs', function() {
+    this.route('blog', {path: 'blogs/:id'});
+  });
 });
 
 export default Router;
