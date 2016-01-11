c76b3773e9aa87e7357e36da70c48a73e19f53b8
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2016-01-10 19:52:16 -0500

    Set up serializer to convert _ to camelCase

--- frontend/app/models/recipe.js
+++ frontend/app/models/recipe.js
@@ -2,6 +2,11 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 
-  name: DS.attr('string')
+  name: 				DS.attr("string"),
+  cookTime: 		DS.attr("string"),
+  recipePath: 	DS.attr("string"),
+  servings: 	  DS.attr("string"),
+  
+  ingredients: DS.hasMany("ingredient")
   
 });
--- frontend/app/serializers/application.js
+++ frontend/app/serializers/application.js
@@ -1,4 +1,13 @@
 import DS from 'ember-data';
 
+let underscore = Ember.String.underscore;
+
 export default DS.JSONAPISerializer.extend({
+  keyForAttribute(key) {
+    return underscore(key);
+  },
+
+  keyForRelationship(rawKey) {
+    return underscore(rawKey);
+  }
 });
