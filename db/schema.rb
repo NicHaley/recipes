22329a257b338f14a398b8abadc05f1d76c1bc6f
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2016-01-26 18:51:38 -0500

    remove name from images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123214117) do
+ActiveRecord::Schema.define(version: 20160126235102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20160123214117) do
   end
 
   create_table "images", force: :cascade do |t|
-    t.string   "name"
     t.integer  "imageable_id"
     t.string   "imageable_type"
     t.datetime "created_at",                  null: false
